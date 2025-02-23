7b7817225290046fa3c1a1e9b866931e34f254dd
upperwal <a.upperwal@gmail.com>
2025-02-23 19:08:44 +0000

    legacy and trainng code

--- pretrain_eka.py
+++ pretrain_eka.py
@@ -1,13 +1,15 @@
 from distributed_utils import init_distributed, destroy_distributed
 from model_provider import model_provider
 from megatron.core.tensor_parallel.random import model_parallel_cuda_manual_seed
+from training.utils import print_rank_0
 
 def main():
     init_distributed()
     model_parallel_cuda_manual_seed(123)
 
     model = model_provider()
-    print(model)
+
+    print_rank_0(model)
     
     destroy_distributed()
 
