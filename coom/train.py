9fa534926e0753e2d5d794f46553e0075d95e0a2
Somay jalan <jalansomay0@gmail.com>
2025-07-15 00:14:31 +0530

    fixing dict and dot change

--- coom/train.py
+++ coom/train.py
@@ -8,39 +8,39 @@ from nemo.collections import llm
 import model
 import config_classes
 
-main_cfg = OmegaConf.load("configs/main_config.yaml")
-prefix = main_cfg.config_path_prefix
+main_cfg = OmegaConf.to_container(OmegaConf.load("configs/main_config.yaml"), resolve=True)
 
-model_cfg = OmegaConf.to_container(OmegaConf.load(os.path.join(prefix, main_cfg.base_model_configuartion_path)), resolve=True)
-data_cfg = OmegaConf.to_container(OmegaConf.load(os.path.join(prefix, main_cfg.dataLoader_config_path)), resolve=True)
-opt_cfg = OmegaConf.to_container(OmegaConf.load(os.path.join(prefix, main_cfg.optimizer_config_path)), resolve=True)
-trainer_cfg = OmegaConf.to_container(OmegaConf.load(os.path.join(prefix, main_cfg.trainer_config_path)), resolve=True)
-logger_cfg = OmegaConf.to_container(OmegaConf.load(os.path.join(prefix, main_cfg.logger_config_path)), resolve=True)
+prefix = main_cfg["config_path_prefix"]
 
-ModelClass = getattr(model, main_cfg.base_model)
-ConfigClass = getattr(config_classes, main_cfg.base_model_config)
+model_cfg = OmegaConf.to_container(OmegaConf.load(os.path.join(prefix, main_cfg["base_model_configuartion_path"])), resolve=True)
+data_cfg = OmegaConf.to_container(OmegaConf.load(os.path.join(prefix, main_cfg["dataLoader_config_path"])), resolve=True)
+opt_cfg = OmegaConf.to_container(OmegaConf.load(os.path.join(prefix, main_cfg["optimizer_config_path"])), resolve=True)
+trainer_cfg = OmegaConf.to_container(OmegaConf.load(os.path.join(prefix, main_cfg["trainer_config_path"])), resolve=True)
+logger_cfg = OmegaConf.to_container(OmegaConf.load(os.path.join(prefix, main_cfg["logger_config_path"])), resolve=True)
+
+ModelClass = getattr(model, main_cfg["base_model"])
+ConfigClass = getattr(config_classes, main_cfg["base_model_config"])
 
 model_config = ConfigClass(**model_cfg)
 modelClasss = ModelClass(model_config)
 
-strategy = nl.MegatronStrategy(**trainer_cfg.strategy)
-
+strategy = nl.MegatronStrategy(**trainer_cfg["strategy"])
 optimizer_config = OptimizerConfig(**opt_cfg)
 optimizer = nl.MegatronOptimizerModule(config=optimizer_config)
 
 trainer = nl.Trainer(
-    devices=trainer_cfg.devices,
-    max_steps=trainer_cfg.max_steps,
-    accelerator=trainer_cfg.accelerator,
+    devices=trainer_cfg["devices"],
+    max_steps=trainer_cfg["max_steps"],
+    accelerator=trainer_cfg["accelerator"],
     strategy=strategy,
-    plugins=nl.MegatronMixedPrecision(precision=trainer_cfg.precision),
+    plugins=nl.MegatronMixedPrecision(precision=trainer_cfg["precision"]),
 )
 
-logger = nl.NeMoLogger(log_dir=logger_cfg.log_dir)
+logger = nl.NeMoLogger(log_dir=logger_cfg["log_dir"])
 
 data = llm.MockDataModule(
-    seq_length=data_cfg.seq_length,
-    global_batch_size=data_cfg.global_batch_size,
+    seq_length=data_cfg["seq_length"],
+    global_batch_size=data_cfg["global_batch_size"],
 )
 
 llm.train(
