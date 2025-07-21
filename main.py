81c15b11002b6ab1c2123f331ac6dc7441e45fad
Aditya <aditya@eka-iitgn.asia-southeast1-c.c.pragna-417707.internal>
2025-07-21 14:29:46 +0000

    added multiprocessing to support docker

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
+import multiprocessing
 from coom.train import Trainer
 
-# Ensure that the configs directory in coom contains a subfolder
-# with the same name, and that it includes the appropriate configuration files.
-experiment_name = "experiment_0"
+def main():
+    # Ensure that the configs directory in coom contains a subfolder
+    # with the same name, and that it includes the appropriate configuration files.
+    experiment_name = "experiment_0"
 
-# This is in case someone wants to do multiple runs with the same configs.
-sub_experiment_name = "trial"
+    # This is in case someone wants to do multiple runs with the same configs.
+    sub_experiment_name = "trial"
 
-trainer = Trainer(
-    experiment_name=experiment_name,
-    sub_experiment_name=sub_experiment_name,
-)
+    trainer = Trainer(
+        experiment_name=experiment_name,
+        sub_experiment_name=sub_experiment_name,
+    )
 
-trainer.train()
+    trainer.train()
+
+if __name__ == "__main__":
+    multiprocessing.set_start_method("spawn", force=True)  
+    main()
